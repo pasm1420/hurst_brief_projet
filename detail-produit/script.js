aae05d01ad06174e295eb95f3b7830479a33eb36
10812632 <khadycisse1420@gmail.com>
2023-10-03 17:37:27 +0000

    03 octobre 2023

---
+++ detail-produit/script.js
@@ -0,0 +1,20 @@
+// Carousel
+var carouselWidth = $(".carousel-inner")[0].scrollWidth; //scrolWidth pour obteni toute la largeur du carrousel
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+$(".carousel-control-next").on("click", function () {
+    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+      scrollPosition += cardWidth;  //update scroll position
+      $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+    }
+  });
+
+  $(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $(".carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
